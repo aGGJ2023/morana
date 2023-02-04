c3aa659ce8936009e6fe0703b779ab62595b3d70
bskracic <borna.skracic7@gmail.com>
2023-02-04 13:53:22 +0100

    initial commit

---
+++ Assets/MasterScripts/GameManager.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    //public Player player;
+    //public FloatingTextManager floatingTextManager;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+}
+
+
